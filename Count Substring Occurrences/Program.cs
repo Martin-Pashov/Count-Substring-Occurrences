3d593aca41001fd60c88efa7b0ca45b453f885a9
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-05-12 09:46:06 +0300

    Add project files.

---
+++ Count Substring Occurrences/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Count_Substring_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some text: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter a pattern: ");
+            string pattern = Console.ReadLine();
+
+            int count = 0;
+            int index = input.IndexOf(pattern);
+
+            while (index != -1)
+            {
+                count++;
+                index = input.IndexOf(pattern, index + 1);
+            }
+            Console.WriteLine($"\r\nThe amount of occurrences is: {count}");
+        }
+    }
+}
